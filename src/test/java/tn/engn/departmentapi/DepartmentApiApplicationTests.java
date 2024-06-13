b389e5ee0e0a020a1c2cb39eb619b0f9167f1743
Chef Service Informatique <engn.si.2022@gmail.com>
2024-06-13 08:53:38 +0100

    initial setup

---
+++ src/test/java/tn/engn/departmentapi/DepartmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.engn.departmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
